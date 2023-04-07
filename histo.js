36aa7965774718246208297ef7a39d29d0bf8d22
Apail59820 <92113742+Apail59820@users.noreply.github.com>
2023-04-07 08:42:47 +0200

    commit #2

--- histo.js
+++ histo.js
@@ -1,5 +1,67 @@
 var data = [];
 
+const form = document.getElementById("filter-form");
+const subtype = form.getAttribute("subtype");
+
+var indexName;
+var fetchUrl;
+var borderColor;
+var backgroundColor;
+
+if(subtype == "temperature"){
+    indexName = "Température (°C)";
+    fetchUrl = "http://localhost:8080/api/entries/temperature";
+    borderColor = 'rgb(255, 99, 132)';
+    backgroundColor = 'rgba(255, 99, 132, 0.2)';
+}
+else if(subtype == "humidity"){
+    fetchUrl = "http://localhost:8080/api/entries/humidity";
+}
+else if(subtype == "co2"){
+    indexName = "CO2 (ppm)";
+    fetchUrl = "http://localhost:8080/api/entries/CO2";
+    borderColor = 'rgb(255, 192, 0)';
+    backgroundColor = 'rgba(255, 192, 0, 0.2)';
+}
+else if(subtype == "no2"){
+    indexName = "NO2 (ppm)";
+    fetchUrl = "http://localhost:8080/api/entries/NO2";
+    borderColor = 'rgb(255, 159, 64)';
+    backgroundColor = 'rgba(255, 159, 64, 0.2)';
+}
+else if(subtype == "cov"){
+    indexName = "COV (ppb)";
+    fetchUrl = "http://localhost:8080/api/entries/COV";
+    borderColor = 'rgb(153, 102, 255)';
+    backgroundColor = 'rgba(153, 102, 255, 0.2)';
+}
+else if(subtype == "pm"){
+    indexName = "Particules Fines (µg/m³)";
+    fetchUrl = "http://localhost:8080/api/entries/PM";
+    borderColor = 'rgb(75, 192, 192)';
+    backgroundColor = 'rgba(75, 192, 192, 0.2)';
+}
+
+var TempValues = [];
+var abs_hum = [];
+
+function computeAbsoluteHumidity(humidity, temperature) {
+    const t = calculateMeanTemperature(temperature, humidity);
+
+    for (let i = 0; i < humidity.length; i++) {
+      const rh = humidity[i].value;
+      const ah = 216.7 * ((rh/100) * 6.112 * Math.exp((17.62*t)/(243.12 + t))) / (273.15 + t);
+      abs_hum.push(ah);
+    }
+}
+  
+function calculateMeanTemperature(temperature, humidity) {
+    const start = humidity[0].timestamp;
+    const end = humidity[humidity.length - 1].timestamp;
+    const tempInRange = temperature.filter((temp) => temp.timestamp >= start && temp.timestamp <= end);
+    const sum = tempInRange.reduce((acc, curr) => acc + curr.value, 0);
+    return sum / tempInRange.length;
+}
 
 function formatTimestamp(timestamp) {
     const date = new Date(timestamp);
@@ -9,23 +71,48 @@ function formatTimestamp(timestamp) {
     return `${hours}:${minutes}:${seconds}`;
 }
 
-function createTemperatureChart(temperatureData, timestampData) {
+function createChart(chartData, timestampData) {
 
     const formattedTimestamps = timestampData.map(timestamp => {
         return formatTimestamp(timestamp);
     });
 
+    let fdatasets = [];
+
+
+    if (subtype === "humidity") {
+        fdatasets.push({
+            label: "Humidité relative (%)",
+            data: chartData,
+            borderColor: 'rgb(99, 99, 255)',
+            backgroundColor: 'rgba(99, 99, 255, 0.2)',
+            yAxisID: 'y-axis-1',
+            tension: 0.2,
+        });
+        fdatasets.push({
+            label: "Humidité absolue (g/m³)",
+            data: abs_hum,
+            borderColor: 'rgb(150, 150, 150)',
+            backgroundColor: 'rgba(150, 150, 150, 0.2)',
+            yAxisID: 'y-axis-2',
+            tension: 0.2,
+        });
+    }
+    else{
+        fdatasets.push({
+            label: indexName,
+            data: chartData,
+            borderColor: borderColor,
+            backgroundColor: backgroundColor,
+            tension: 0.4,
+        });
+    }
+
     const chart = new Chart('chart', {
         type: 'line',
         data: {
             labels: formattedTimestamps,
-            datasets: [{
-                label: 'Température',
-                data: temperatureData,
-                borderColor: 'rgb(255, 99, 132)',
-                backgroundColor: 'rgba(255, 99, 132, 0.2)',
-                tension: 0.4,
-            }]
+            datasets: fdatasets
         },
         options: {
             scales: {
@@ -53,12 +140,12 @@ function computeStats(data, startTimestamp, endTimestamp) {
         return elem.timestamp >= startTimestamp && elem.timestamp <= endTimestamp;
     });
 
-    const maxTemp = Math.max(...filteredData.map((elem) => elem.temperature));
+    const maxTemp = Math.max(...filteredData.map((elem) => elem.value));
 
-    const minTemp = Math.min(...filteredData.map((elem) => elem.temperature));
+    const minTemp = Math.min(...filteredData.map((elem) => elem.value));
 
     const avgTemp =
-        filteredData.reduce((sum, elem) => sum + elem.temperature, 0) /
+        filteredData.reduce((sum, elem) => sum + elem.value, 0) /
         filteredData.length;
 
     return {
@@ -69,68 +156,81 @@ function computeStats(data, startTimestamp, endTimestamp) {
 }
 
 function loadDBValues() {
-    fetch('http://localhost:8080/api/entries/temperature')
+
+    fetch("http://localhost:8080/api/entries/temperature")
         .then(response => response.json())
         .then(dbData => {
-            const newData = dbData.map(row => {
+            const tData = dbData.map(row => {
                 return {
-                    temperature: row.value,
+                    value: row.value,
                     timestamp: new Date(row.createdAt).getTime()
                 }
             });
-            data = newData;
+            TempValues = tData;
+            fetch(fetchUrl)
+                .then(response => response.json())
+                .then(dbData => {
+                    const newData = dbData.map(row => {
+                        return {
+                            value: row.value,
+                            timestamp: new Date(row.createdAt).getTime()
+                        }
+                    });
+                    data = newData;
+                    
+                    if(subtype == "humidity")
+                        computeAbsoluteHumidity(newData, TempValues);
 
-            const temperatureData = data.map(entry => entry.temperature);
-            const timestampData = data.map(entry => entry.timestamp);
-            tempchart = createTemperatureChart(temperatureData, timestampData);
+                    const chartData = data.map(entry => entry.value);
+                    const timestampData = data.map(entry => entry.timestamp);
+                    mychart = createChart(chartData, timestampData);
 
-            const timestamps = data.map(obj => obj.timestamp);
+                    const timestamps = data.map(obj => obj.timestamp);
 
-            for (let ts = 0; ts < timestamps.length; ts++) {
-                const option = document.createElement('option');
-                option.text = new Date(timestamps[ts]).toLocaleString('fr-FR', {
-                    day: 'numeric',
-                    month: 'numeric',
-                    hour: 'numeric',
-                    minute: 'numeric',
-                    second: 'numeric'
+                    for (let ts = 0; ts < timestamps.length; ts++) {
+                        const option = document.createElement('option');
+                        option.text = new Date(timestamps[ts]).toLocaleString('fr-FR', {
+                            day: 'numeric',
+                            month: 'numeric',
+                            hour: 'numeric',
+                            minute: 'numeric',
+                            second: 'numeric'
+                        });
+                        option.value = timestamps[ts];
+                        selectStart.add(option);
+
+
+                        const endOption = document.createElement('option');
+                        endOption.text = new Date(timestamps[ts]).toLocaleString('fr-FR', {
+                            day: 'numeric',
+                            month: 'numeric',
+                            hour: 'numeric',
+                            minute: 'numeric',
+                            second: 'numeric'
+                        });
+                        endOption.value = timestamps[ts];
+                        selectEnd.add(endOption);
+                    }
+
+                    selectStart.value = timestamps[0];
+
+                    selectEnd.value = timestamps[timestamps.length - 1];
+
+                    let stats = computeStats(data, timestamps[0], timestamps[timestamps.length - 1]);
+
+                    var peakTemp = stats.maxTemp;
+                    var avgTemp = stats.avgTemp;
+                    var minTemp = stats.minTemp;
+
+                    var peakSpan = document.getElementById("peakvalue");
+                    var avgSpan = document.getElementById("avgvalue");
+                    var minSpan = document.getElementById("minvalue");
+
+                    peakSpan.innerHTML = peakTemp;
+                    avgSpan.innerHTML = avgTemp.toFixed(2);
+                    minSpan.innerHTML = minTemp;
                 });
-                option.value = timestamps[ts];
-                selectStart.add(option);
-
-
-                const endOption = document.createElement('option');
-                endOption.text = new Date(timestamps[ts]).toLocaleString('fr-FR', {
-                    day: 'numeric',
-                    month: 'numeric',
-                    hour: 'numeric',
-                    minute: 'numeric',
-                    second: 'numeric'
-                });
-                endOption.value = timestamps[ts];
-                selectEnd.add(endOption);
-            }
-
-            selectStart.value = timestamps[0];
-
-            selectEnd.value = timestamps[timestamps.length - 1];
-
-            let stats = computeStats(data, timestamps[0], timestamps[timestamps.length - 1]);
-
-            var peakTemp = stats.maxTemp;
-            var avgTemp = stats.avgTemp;
-            var minTemp = stats.minTemp;
-
-            var peakSpan = document.getElementById("peakvalue");
-            var avgSpan = document.getElementById("avgvalue");
-            var minSpan = document.getElementById("minvalue");
-
-            peakSpan.innerHTML = peakTemp;
-            avgSpan.innerHTML = avgTemp.toFixed(2);
-            minSpan.innerHTML = minTemp;
-
-        })
-        .catch(error => console.error(error));
+    }).catch(error => console.error(error));
 }
 loadDBValues();
 
@@ -142,7 +242,7 @@ function updateChartData(chart, data, startDate, endDate) {
         return itemDate >= new Date(parseInt(startDate)) && itemDate <= new Date(parseInt(endDate));
     });
 
-    chart.data.datasets[0].data = filteredData.map((item) => item.temperature);
+    chart.data.datasets[0].data = filteredData.map((item) => item.value);
     chart.data.labels = filteredData.map((item) => formatTimestamp(item.timestamp));
     chart.update();
 }
@@ -173,5 +273,5 @@ document.getElementById('filter-form').addEventListener('change', (event) => {
     avgSpan.innerHTML = avgTemp.toFixed(2);
     minSpan.innerHTML = minTemp;
 
-    updateChartData(tempchart, data, startDate, endDate);
+    updateChartData(mychart, data, startDate, endDate);
 });
--- index.php
+++ index.php
@@ -38,7 +38,7 @@
                             Humidité Relative : 25%<br>
                             Humidité Absolue : 1200 g/m³
                         </p>
-                        <button class="historique">Historique</button>
+                        <button class="historique" id="hum_histo">Historique</button>
                     </div>
                     <div class = "capteur">
                         <img src="images/co2.png" alt="image">
@@ -46,7 +46,7 @@
                         <p>
                             Valeur Relevée : 500ppm
                         </p>
-                        <button class="historique">Historique</button>
+                        <button class="historique" id="co2_histo">Historique</button>
                     </div>
                     <div class = "capteur">
                         <img src="images/no2.png" alt="image">
@@ -54,7 +54,7 @@
                         <p>
                             Valeur Relevée : 200ppm
                         </p>
-                        <button class="historique">Historique</button>
+                        <button class="historique" id="no2_histo">Historique</button>
                     </div>
                     <div class = "capteur">
                         <img src="images/cov.png" alt="image">
@@ -62,7 +62,7 @@
                         <p>
                             Valeur Relevée : 1700ppb
                         </p>
-                        <button class="historique">Historique</button>
+                        <button class="historique" id="cov_histo">Historique</button>
                     </div>
                     <div class = "capteur">
                         <img src="images/pm.png" alt="image">
@@ -72,43 +72,100 @@
                             Particules 5µm : 240µg/m³
                             Particules 10µm : 150µg/m³
                         </p>
-                        <button class="historique">Historique</button>
+                        <button class="historique" id="pm_histo">Historique</button>
                     </div>
                 </div>
 
-                <?php } elseif ($tab == "temp_histo") { ?>
+                <?php } elseif (
+                $tab == "history" &&
+                isset($_GET["type"]) &&
+                !empty($_GET["type"])
+            ) {
+                $iType = $_GET["type"]; ?>
                     <div style="display: flex;">
+
+                    <?php switch ($iType) {
+                        case "temperature":
+                            $unit = "°C";
+                            break;
+                        case "humidity":
+                            $unit = "%";
+                            break;
+                        case "co2":
+                            $unit = "ppm";
+                            break;
+                        case "no2":
+                            $unit = "ppm";
+                            break;
+                        case "cov":
+                            $unit = "ppb";
+                            break;
+                        case "pm":
+                            $unit = "µg/m³";
+                            break;
+                    } ?>
                         <canvas id="chart" width="1200" height="600" style="margin-top: 50px; margin-right: 50px; margin-left: 50px;"></canvas>
                             <div style="display: flex; flex-direction: column; justify-content: center;">
-                                <form id="filter-form" style="display: flex; align-items: center; margin-right: 50px; margin-top: 50px;">
-                                <label for="select-start">Début</label>
-                                <select id="select-start" name="select-start" style="margin-right: 10px; padding: 5px; border-radius: 5px; border: 1px solid #a9c9ff;"></select>
+                                <form id="filter-form" style="display: flex; align-items: center; margin-right: 50px; margin-top: 50px;" subtype=<?php echo '"' .
+                                    $iType .
+                                    '"'; ?>>
+                                    <label for="select-start">Début</label>
+                                    <select id="select-start" name="select-start" style="margin-right: 10px; padding: 5px; border-radius: 5px; border: 1px solid #a9c9ff;"></select>
 
-                                <label for="select-end">Fin</label>
-                                <select id="select-end" name="select-end" style="padding: 5px; border-radius: 5px; border: 1px solid #a9c9ff;"></select>
+                                    <label for="select-end">Fin</label>
+                                    <select id="select-end" name="select-end" style="padding: 5px; border-radius: 5px; border: 1px solid #a9c9ff;"></select>
                                 </form> 
-                                <h3 style='font-family: "Poppins", sans-serif;'>Pic de Température : <span class="formtext" id="peakvalue"></span></h3>
-                                <h3 style='font-family: "Poppins", sans-serif;'>Température moyenne : <span id="avgvalue"></span></h3>
-                                <h3 style='font-family: "Poppins", sans-serif;'>Température minimum : <span id="minvalue"></span></h3>
+                                <h3 style='font-family: "Poppins", sans-serif;'>Valeur Pic : <span class="formtext" id="peakvalue"></span><?php echo $unit; ?></h3>
+                                <h3 style='font-family: "Poppins", sans-serif;'>Valeur Moyenne : <span id="avgvalue"></span><?php echo $unit; ?></h3>
+                                <h3 style='font-family: "Poppins", sans-serif;'>Valeur Minimum : <span id="minvalue"></span><?php echo $unit; ?></h3>
                             </div>
                         </div>
                                     
-                <?php }
+                <?php
+            }
         } ?>
     </body>
     <script>
 
         const temp_histo_btn = document.querySelector('#temp_histo');
+        const hum_histo_btn = document.querySelector('#hum_histo');
+        const co2_histo_btn = document.querySelector('#co2_histo');
+        const no2_histo_btn = document.querySelector('#no2_histo');
+        const cov_histo_btn = document.querySelector('#cov_histo');
+        const pm_histo_btn = document.querySelector('#pm_histo');
 
         function temp_histo_clicked() {
-            location.href = '?tab=temp_histo';
+            location.href = '?tab=history&type=temperature';
+        }
+		function hum_histo_clicked() {
+            location.href = '?tab=history&type=humidity';
+        }
+		function co2_histo_clicked() {
+            location.href = '?tab=history&type=co2';
+        }
+		function no2_histo_clicked() {
+            location.href = '?tab=history&type=no2';
+        }
+		function cov_histo_clicked() {
+            location.href = '?tab=history&type=cov';
+        }
+		function pm_histo_clicked() {
+            location.href = '?tab=history&type=pm';
         }
 		
 		if(temp_histo_btn)
-		{
 			temp_histo_btn.addEventListener("click", temp_histo_clicked);
-		}
-		
+        if(hum_histo_btn)
+			hum_histo_btn.addEventListener("click", hum_histo_clicked);
+        if(co2_histo_btn)
+			co2_histo_btn.addEventListener("click", co2_histo_clicked);
+        if(no2_histo_btn)
+			no2_histo_btn.addEventListener("click", no2_histo_clicked);           
+        if(cov_histo_btn)
+			cov_histo_btn.addEventListener("click", cov_histo_clicked);
+        if(pm_histo_btn)
+			pm_histo_btn.addEventListener("click", pm_histo_clicked);
+
         window.addEventListener('load', function() {
             var capteurs = document.querySelectorAll('.capteur');
             for (var i = 0; i < capteurs.length; i++) {
